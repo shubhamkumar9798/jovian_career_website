5d7b295cf2fa93cda15d883676508ac48e0406fa
shubhamkumar9798 <149182027+shubhamkumar9798@users.noreply.github.com>
2024-10-11 14:18:08 +0000

    Added elements, css and html.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask    #Flask is a class inside flask 
+from flask import Flask, render_template    #Flask is a class inside flask and render_template is a function inside flask to access the html file
 
 app = Flask(__name__)      #app is an object of the class Flask
 
 #url
 @app.route("/")         # @ is called decorator in py
 def hello_world():
-    return "<p>Hello, World!</p>"  #when the url / is called, it will return this
+    return render_template("index.html")  #when the url / is called, it will return this
 
 # to run the flask file
 if __name__ == "__main__":
